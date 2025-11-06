3cda21106fa08f32e6ade0d6142c55d219a550fd
Jungkyu Park <jungkyupark@microsoft.com>
2025-11-06 10:31:32 +0900

    add log

--- helloworld/src/main/java/com/example/MyApplication.java
+++ helloworld/src/main/java/com/example/MyApplication.java
@@ -1,5 +1,8 @@
 package com.example;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MyApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(MyApplication.class);
+
 	@RequestMapping("/")
 	String home() {
+		logger.debug("debug hello");
+		logger.info("info");
 		return "Hello World!";
 	}
 
 	@RequestMapping("/sleep")
 	String sleep() {
 		try {
+        	logger.debug("start sleep");
             Thread.sleep(100_000); // 100초 (100,000ms)
+			logger.debug("end sleep");
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             return "Interrupted!";
